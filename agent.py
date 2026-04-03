c2b9c8bbf2690325ab8de2b83ec3e393d1e97631
mokshitvora <138001979+mokshitvora@users.noreply.github.com>
2026-04-03 23:58:22 +0530

    Day 4 - Tavily search tool working

--- agent.py
+++ agent.py
@@ -2,7 +2,8 @@ from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 search = TavilySearch(max_results=3, tavily_api_key=os.getenv("TAVILY_API_KEY"))
 
 result = search.run("Nifty 50 latest news today")
