afbe78f8da4c387b9e9e3dfaa8f41dc48644162c
mokshitvora <138001979+mokshitvora@users.noreply.github.com>
2026-04-09 21:29:48 +0530

    Day 4 - Tavily search tool working

--- basics2.py
+++ basics2.py
@@ -4,13 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-    return response.choices[0].message.content
-
-
     
-print(ask("You are a helpful assistant", "llama-3.3-70b-versatile", "what is yellow?"))
-
-
 
 
 
